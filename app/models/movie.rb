0ba728d1b89f7249a7bd877a5fedfdcb2baa0b5e
oleg deru <olegderu98@gmail.com>
2022-02-17 12:48:45 +0100

    models & schema done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,5 @@
 class Movie < ApplicationRecord
-  belongs_to :movie
-  belongs_to :list
-  validates :comment, :list, presence: true
-  validates :title, presence: true
+  has_many :bookmarks
+  validates :title, :overview, presence: true
+  validates :title, uniqueness: true
 end
